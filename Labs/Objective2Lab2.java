24299450dcf80f544ac7a691b503f4ac8fbddd99
Kbow009 <bowman.kristin@gmail.com>
2021-07-02 11:32:47 -0700

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Kristin Bowman";
+    int age = 47;
+    String state = "California";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+
+}
